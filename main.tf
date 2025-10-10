28dda63a202646a3255df3f08c2c99fb48169a22
karthikeyan <karthikeyangopal16@gmail.com>
2025-10-10 11:11:44 +0200

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 module "vpc" {
     source = "./modules/vpc"
-    vpc = var.vpc
-    vpc_name   = var.vpc[vpc_name]
-    cidr_block = var.vpc[cidr_block]
+      cidr = var.vpc["cidr"]
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "bastion_app_vpc" {
-    count      = length(var.vpc.vpc_name) 
+    count      = length(var.vpc.[vpc_name]) 
     cidr_block = var.vpc.cidr_block[count.index]
 
 tags = {
